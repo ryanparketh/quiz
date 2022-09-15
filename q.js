f80bc4c1e16bb16482b1ed43a5b072503be89cb3
ryanparketh <ryanpark@rocketmail.com>
2022-09-14 23:23:32 -0400

    added q.js and script.js need to work timer though

---
+++ q.js
@@ -0,0 +1,35 @@
+let questions = [
+    {
+    numb: 1,
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answer: "Console log",
+    options: [
+      "JavaScript",
+      "Terminal/Bash",
+      "For Loops",
+      "Console log"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Commonly used data types DO Not include:",
+    answer: "Alerts",
+    options: [
+      "Strings",
+      "Booleans",
+      "Alerts",
+      "Numbers"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The condition in an if/else statment is enclosed within?",
+    answer: "Curly Brackets",
+    options: [
+      "Curly Brackets",
+      "Square Brackets",
+      "Quotes",
+      "Paraentheses"
+    ]
+  },
+];
